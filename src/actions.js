36f6eac35888fa6d65337817133185ea977cf7a8
Victoria <victoria@hostastay.com>
2019-10-19 20:06:11 +0800

    change updates to object of arrays

--- src/actions.js
+++ src/actions.js
@@ -26,7 +26,7 @@ const resourcesState = {
   employee: 0,
   gg: 1,
   seaLevel: 0,
-  temperature: 28
+  temperature: 10
 };
 
 const resourceMultipliers = {
@@ -52,10 +52,10 @@ const resourceMultipliers = {
   }
 };
 
-const calculateEnvironmentalChanges = () => {
+const updateEnvironmentalChanges = () => {
   const { gg } = resourcesState;
-  resourcesState.temperature = gg / 20000 + 28;
-  resourcesState.seaLevel = (resourcesState.temperature - 28) * 5;
+  resourcesState.temperature = gg / 20000 + 10;
+  resourcesState.seaLevel = (resourcesState.temperature - 10) * 5;
 };
 
 const handleAction = actionName => {
@@ -67,26 +67,16 @@ const handleAction = actionName => {
 const handleSellButton = () => {
   const { money, patty, gg } = resourcesState;
 
-  calculateEnvironmentalChanges();
-
-  const updatedState = {
-    money: money + patty * pricePerPatty,
-    patty: 0,
-    energy: energyCap,
-    seaLevel,
-    temperature
-  };
+  resourcesState.money = money + patty * pricePerPatty;
+  resourcesState.patty = 0;
+  resourcesState.energy = energyCap;
 
   if (Math.random() < 0.2 && gg > 1) {
-    updatedState.gg = gg - 1;
+    resourcesState.gg = gg - 1;
   } else {
-    updatedState.gg = gg + 1;
+    resourcesState.gg = gg + 1;
   }
-
-  resourcesState = {
-    ...resourcesState,
-    ...updatedState
-  };
+  updateEnvironmentalChanges();
 };
 
 module.exports = {
@@ -95,49 +85,99 @@ module.exports = {
   handleSellButton
 };
 
-const upgrades = [
-  {
-    label: 'Improve Meat Quality',
-    desc: 'RM 1000++ each, increase Buy Cow and Sell Patty price, energy usage, increase GG impact for Buy Cow (a)',
-    stage: 1,
-    actionUsed: 0,
-    costIncrement: 500,
-    cost: 1000,
-    updateResourceMultiplier: function() {
-      const { buyCow } = resourceMultipliers;
-      buyCow.money = buyCow.money * 1.01;
-      buyCow.energy = buyCow.energy * 1.01;
-      buyCow.gg = buyCow.gg * 1.01;
-      pricePerPatty = pricePerPatty * 1.01;
-      this.actionUsed++;
-      resourcesState.money = resourcesState.money - this.cost;
-      this.cost = this.cost + this.costIncrement;
+const upgrades = {
+  two: [
+    {
+      // Improve meat quality - RM 1000++ each, increase Buy Cow and Sell Patty price, energy usage, increase GG impact for Buy Cow (a)
+      label: 'Improve meat quality',
+      desc: 'RM 1000++ each, increase Buy Cow and Sell Patty price, energy usage, increase GG impact for Buy Cow (a)',
+      stage: 2,
+      actionUsed: 0,
+      costMultiplier: 1.5,
+      cost: 1000,
+      updateResourceMultiplier: function() {
+        const { buyCow } = resourceMultipliers;
+        buyCow.money = buyCow.money * 1.01;
+        buyCow.energy = buyCow.energy * 1.01;
+        buyCow.gg = buyCow.gg * 1.01;
+        pricePerPatty = pricePerPatty * 1.01;
+        this.actionUsed++;
+        resourcesState.money = resourcesState.money - this.cost;
+        this.cost = this.cost * this.costMultiplier;
+      }
+    },
+    {
+      // Reduce Process Cost - RM 2000++ each, reduce Process Cow price, increase energy usage, increase GG impact (a)
+      label: 'Reduce Process Cost',
+      desc: 'RM 2000++ each, reduce Process Cow price, increase energy usage, increase GG impact (a)',
+      stage: 2,
+      actionUsed: 0,
+      costMultiplier: 1.5,
+      cost: 2000,
+      updateResourceMultiplier: function() {
+        const { processCow } = resourceMultipliers;
+        processCow.money = processCow.money * 0.99;
+        processCow.energy = processCow.energy - 2;
+        processCow.gg = processCow.gg * 1.1;
+        this.actionUsed++;
+        resourcesState.money = resourcesState.money - this.cost;
+        this.cost = this.cost * this.costMultiplier;
+      }
+    },
+    {
+      // Red Bull - RM 5000++ each, increase energy by 5
+      label: 'Red Bull',
+      desc: 'RM 5000++ each, increase energy by 5',
+      stage: 2,
+      actionUsed: 0,
+      costMultiplier: 1.5,
+      cost: 5000,
+      updateResourceMultiplier: function() {
+        const { packagePatty } = resourceMultipliers;
+        packagePatty.money = packagePatty.money * 0.99;
+        pricePerPatty = pricePerPatty * 0.01;
+        this.actionUsed++;
+        resourcesState.money = resourcesState.money - this.cost;
+        this.cost = this.cost * this.costMultiplier;
+      }
+    },
+    {
+      // Strike A Deal - RM5,000, increase Buy Cow price by 1.8, get 2 cows each time, single usage
+      label: 'Strike A Deal',
+      desc: 'RM5,000, increase Buy Cow price by 1.8, get 2 cows each time, single usage',
+      stage: 2,
+      actionUsed: 0,
+      costMultiplier: 1.5,
+      cost: 2000,
+      updateResourceMultiplier: function() {
+        const { packagePatty } = resourceMultipliers;
+        packagePatty.money = packagePatty.money * 0.99;
+        pricePerPatty = pricePerPatty * 0.01;
+        this.actionUsed++;
+        resourcesState.money = resourcesState.money - this.cost;
+        this.cost = this.cost * this.costMultiplier;
+      }
+    },
+    {
+      // Build Workshop - RM 10,000, halve Process and Package costs, energy usage, single purchase, increase GG impact (m)
+      label: 'Build Workshop',
+      desc: 'RM 10,000, halve Process and Package costs, energy usage, single purchase, increase GG impact (m)',
+      stage: 2,
+      actionUsed: 0,
+      costMultiplier: 1.5,
+      cost: 2000,
+      updateResourceMultiplier: function() {
+        const { packagePatty } = resourceMultipliers;
+        packagePatty.money = packagePatty.money * 0.99;
+        pricePerPatty = pricePerPatty * 0.01;
+        this.actionUsed++;
+        resourcesState.money = resourcesState.money - this.cost;
+        this.cost = this.cost * this.costMultiplier;
+      }
     }
-  },
-  {
-    label: 'Reduce Process Cost',
-    desc: 'RM 2000++ each, reduce Process Cow price, increase energy usage, increase GG impact (a)',
-    stage: 1,
-    actionUsed: 0,
-    costIncrement: 1000,
-    cost: 2000,
-    updateResourceMultiplier: function() {
-      const { packagePatty } = resourceMultipliers;
-      packagePatty.money = packagePatty.money * 0.99;
-      pricePerPatty = pricePerPatty * 0.01;
-      this.actionUsed++;
-      resourcesState.money = resourcesState.money - this.cost;
-      this.cost = this.cost + this.costIncrement;
-    }
-  }
-];
+  ]
+};
 
-// Improve meat quality - RM 1000++ each, increase Buy Cow and Sell Patty price, energy usage, increase GG impact for Buy Cow (a)
-// Reduce Process Cost - RM 2000++ each, reduce Process Cow price, increase energy usage, increase GG impact (a)
-// Reduce Package - RM 2000++ each, reduce Package Patty price, increase energy usage, increase GG impact (a)
-// Red Bull - RM 5000++ each, increase energy by 5
-// Strike A Deal - RM5,000, increase Buy Cow price by 1.8, get 2 cows each time, single usage
-// Build Workshop - RM 10,000, halve Process and Package costs, energy usage, single purchase, increase GG impact (m)
 // Purchase Advertisements - RM 30,000, increase Sell Patty price by 50% additively, single purchase
 // Set up Company - RM 20,000, requires Build Workshop, unlock Public Relations, third stage
 // Hire Employee - RM 10,000++ each, reduce energy usage, max 10
